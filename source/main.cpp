414d67a3202f3954e28bcdbcce7b14f1ed1d0818
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-05-17 18:32:36 +0200

    Add more descriptions to tests

--- source/main.cpp
+++ source/main.cpp
@@ -12,9 +12,9 @@ int main(void) {
 	{
 		Test test("Constructor Fill: Fill the list with an item");
 		ft::vector<std::string> lst(3, "Works though");
-		test.notnull(lst[0], "is lst[0] NULL");
-		test.notnull(lst[1], "is lst[1] NULL");
-		test.notnull(lst[2], "is lst[2] NULL");;
+		test.notnull(lst[0], "is lst[0] not NULL");
+		test.notnull(lst[1], "is lst[1] not NULL");
+		test.notnull(lst[2], "is lst[2] not NULL");;
 		test.equal(lst[0], std::string("Works though"), "is lst[0] right content");
 		test.equal(lst[1], std::string("Works though"), "is lst[1] right content");
 		test.equal(lst[2], std::string("Works though"), "is lst[1] right content");
@@ -23,42 +23,42 @@ int main(void) {
 		Test test("push_back(): add element to end");
 		ft::vector<std::string> lst;
 		lst.push_back("Hello World!");
-		test.notnull(lst[0]);
-		test.equal(lst[0], std::string("Hello World!"));
+		test.notnull(lst[0], "is lst[0] not NULL");
+		test.equal(lst[0], std::string("Hello World!"), "is lst[0] right content");
 		lst.push_back("Hi there!");
-		test.notnull(lst[1]);
-		test.equal(lst[1], std::string("Hi there!"));
+		test.notnull(lst[1], "is lst[1] not NULL");
+		test.equal(lst[1], std::string("Hi there!"), "is lst[1] right content");
 		lst.push_back("Ok cool");
-		test.notnull(lst[2]);
-		test.equal(lst[2], std::string("Ok cool"));
+		test.notnull(lst[2], "is lst[2] not NULL");
+		test.equal(lst[2], std::string("Ok cool"), "is lst[2] right content");
 	}
 	{
 		Test test("empty(): check if list is empty");
 		ft::vector<std::string> lst;
-		test.equal(lst.empty(), true);
+		test.equal(lst.empty(), true, "is lst empty");
 		lst.push_back("Hi there");
-		test.equal(lst.empty(), false);
+		test.equal(lst.empty(), false, "is lst non-empty");
 	}
 	{
 		Test test("size(): check list size");
 		ft::vector<std::string> lst;
-		test.equal(lst.size(), (size_t)0);
+		test.equal(lst.size(), (size_t)0, "is lst size 0");
 		lst.push_back("Hi there");
-		test.equal(lst.size(), (size_t)1);
+		test.equal(lst.size(), (size_t)1, "is lst size 1");
 		lst.push_back("Hi there");
-		test.equal(lst.size(), (size_t)2);
+		test.equal(lst.size(), (size_t)2, "is lst size 2");
 	}
 	{
 		Test test("front(): get first item");
 		ft::vector<std::string> lst;
 		lst.push_back("Hello World!");
 		lst.push_back("Hi there!");
-		test.equal(lst.front(), std::string("Hello World!"));
+		test.equal(lst.front(), std::string("Hello World!"), "is first element correct");
 	}
 	{
 		Test test("get_allocator(): get the allocator");
 		ft::vector<std::string> lst;
-		test.equal(lst.get_allocator(), std::allocator<std::string>());
+		test.equal(lst.get_allocator(), std::allocator<std::string>(), "is allocator std::allocator");
 	}
 	Test test;
 	test.summary();
