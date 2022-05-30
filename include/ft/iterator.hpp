bb022acaa8708497b20cd6a26f5f60b41691b571
GQDeltex <operitum@gmail.com>
2022-05-30 15:42:03 +0200

    Start work on custom iterator

---
+++ include/ft/iterator.hpp
@@ -0,0 +1,37 @@
+#ifndef FT_ITERATOR_H
+# define FT_ITERATOR_H
+
+#include <iterator>
+
+namespace ft {
+	template <
+		class T
+	> class iterator : std::iterator<std::random_access_iterator_tag, T> {
+		public:
+			typedef typename std::iterator<std::random_access_iterator_tag, T>::iterator_category	iterator_category;
+			typedef typename std::iterator<std::random_access_iterator_tag, T>::value_type			value_type;
+			typedef typename std::iterator<std::random_access_iterator_tag, T>::difference_type		difference_type;
+			typedef typename std::iterator<std::random_access_iterator_tag, T>::pointer				pointer;
+			typedef typename std::iterator<std::random_access_iterator_tag, T>::reference			reference;
+		private:
+			value_type *_ptr;
+		public:
+			iterator(value_type *in) {
+				_ptr = in;
+			}
+			const value_type&	operator*() const {
+				return *_ptr;
+			}
+			iterator	operator++() {
+				_ptr++;
+				return *this;
+			}
+			iterator	operator++(int) {
+				ft::iterator<T> temp = *this;
+				_ptr++;
+				return temp;
+			}
+	};
+};
+
+#endif
