b4d3af906842123a3b8407adc7585d925a7207c0
GQDeltex <operitum@gmail.com>
2022-05-31 15:52:59 +0200

    Add equal and unequal operators to iterator

--- include/ft/iterator.hpp
+++ include/ft/iterator.hpp
@@ -15,39 +15,45 @@ namespace ft {
 			typedef typename std::iterator<std::random_access_iterator_tag, T>::pointer				pointer;
 			typedef typename std::iterator<std::random_access_iterator_tag, T>::reference			reference;
 		protected:
-			value_type *_ptr;
+			value_type	*_ptr;
 		public:
-			iterator() {
-				_ptr = NULL;
-			}
-			iterator(value_type *in) {
-				_ptr = in;
-			}
-			iterator(const iterator& it) {
-				*this = it;
-			}
-			const iterator& operator=(const iterator& it) {
-				this->_ptr = it._ptr;
-				return *this;
-			}
+								iterator() {
+									_ptr = NULL;
+								}
+								iterator(value_type *in) {
+									_ptr = in;
+								}
+								iterator(const iterator& it) {
+									*this = it;
+								}
+			const iterator&		operator=(const iterator& it) {
+									this->_ptr = it._ptr;
+									return *this;
+								}
 			const value_type&	operator*() const {
-				if (_ptr == NULL)
-					throw std::runtime_error("Cannot dereference NULL Pointer");
-				return *_ptr;
-			}
-			iterator	operator++() {
-				if (_ptr == NULL)
-					throw std::runtime_error("Cannot dereference NULL Pointer");
-				_ptr++;
-				return *this;
-			}
-			iterator	operator++(int) {
-				if (_ptr == NULL)
-					throw std::runtime_error("Cannot dereference NULL Pointer");
-				ft::iterator<T> temp = *this;
-				_ptr++;
-				return temp;
-			}
+									if (_ptr == NULL)
+										throw std::runtime_error("Cannot dereference NULL Pointer");
+									return *_ptr;
+								}
+			iterator			operator++() {
+									if (_ptr == NULL)
+										throw std::runtime_error("Cannot dereference NULL Pointer");
+									_ptr++;
+									return *this;
+								}
+			iterator			operator++(int) {
+									if (_ptr == NULL)
+										throw std::runtime_error("Cannot dereference NULL Pointer");
+									ft::iterator<T> temp = *this;
+									_ptr++;
+									return temp;
+								}
+			friend bool			operator==(const iterator<T>& lhs, const iterator<T>& rhs) {
+									return (lhs._ptr == rhs._ptr);
+								}
+			friend bool			operator!=(const iterator<T>& lhs, const iterator<T>& rhs) {
+									return !(lhs == rhs);
+								}
 	};
 };
 
