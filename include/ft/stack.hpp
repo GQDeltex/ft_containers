bbbca48c973b7580442704f773b7993bd0354fdc
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-06-11 15:52:43 +0200

    Add empty,size,top,push and pop

--- include/ft/stack.hpp
+++ include/ft/stack.hpp
@@ -18,11 +18,36 @@ namespace ft {
 
 		public:
 			// Constructors
-			stack (
-				const container_type& ctnr = container_type()
-			) {
-				this->cont = ctnr;
-			}
+								stack (
+									const container_type& ctnr = container_type()
+								) {
+									this->cont = ctnr;
+								}
+			// empty
+			bool				empty() const {
+									return this->cont.empty();
+								}
+			// size
+			size_type			size() const {
+									return this->cont.size();
+								}
+			// top
+			value_type&			top() {
+									return this->cont.back();
+								}
+			const value_type&	top() const {
+									return this->cont.back();
+								}
+			// push
+			void				push(
+									const value_type& val
+								) {
+									this->cont.push_back(val);
+								}
+			// pop
+			void				pop() {
+									this->cont.pop_back();
+								}
 	};
 }
 
--- source/stack.cpp
+++ source/stack.cpp
@@ -1,7 +1,7 @@
 #include "Test.hpp"
 
 #ifndef USE_STD
-# define USE_STD 1
+# define USE_STD 0
 #endif
 
 #if USE_STD == 1
@@ -15,6 +15,37 @@ void TestStack() {
 	{
 		Test test("Default Constructor");
 		ft::stack<std::string> stk;
+	}
+	{
+		Test test("Stack empty");
+		ft::stack<std::string> stk;
 		test.equal(stk.empty(), true);
+		stk.push("Hello World!");
+		test.equal(stk.empty(), false);
+	}
+	{
+		Test test("Stack size");
+		ft::stack<std::string> stk;
+		test.equal(stk.size(), (size_t)0);
+		stk.push("Hello World");
+		test.equal(stk.size(), (size_t)1);
+	}
+	{
+		Test test("Stack top");
+		ft::stack<std::string> stk;
+		stk.push("Hello World");
+		test.equal(stk.top(), std::string("Hello World"), "First element top");
+		stk.push("Hi there");
+		test.equal(stk.top(), std::string("Hi there"), "Second element top");
+	}
+	{
+		Test test("Stack pop");
+		ft::stack<std::string> stk;
+		stk.push("Hello World");
+		test.equal(stk.top(), std::string("Hello World"), "First element top");
+		stk.push("Hi there");
+		test.equal(stk.top(), std::string("Hi there"), "Second element top");
+		stk.pop();
+		test.equal(stk.top(), std::string("Hello World"), "First back on top");
 	}
 }
