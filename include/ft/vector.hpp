31caf4ec695690a5ab6122da6703bb023435a16f
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-06-22 10:42:15 +0200

    Fix spacing

--- include/ft/vector.hpp
+++ include/ft/vector.hpp
@@ -25,7 +25,7 @@ namespace ft {
 			typedef typename allocator_type::const_reference	const_reference;
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
-			typedef ft::vector_iterator<T>								iterator;
+			typedef ft::vector_iterator<T>						iterator;
 			typedef const iterator								const_iterator;
 			typedef typename ft::reverse_iterator<iterator>		reverse_iterator;
 			typedef const reverse_iterator						const_reverse_iterator;
