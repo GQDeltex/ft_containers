cc1c3386b84b8e30804615d8de93debf5b3defe1
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-07-01 10:45:09 +0200

    Add more functionality to rbtree_iterator

--- include/ft/rbtree_iterator.hpp
+++ include/ft/rbtree_iterator.hpp
@@ -1,33 +1,81 @@
 #ifndef FT_SET_ITERATOR_H
 # define FT_SET_ITERATOR_H
 
+# include <functional>
 # include <iterator>
+# include <stdexcept>
+# include "rbtree.hpp"
+
 
 namespace ft {
 	template<
-		typename T
+		typename T,
+		typename Comp = std::less<T>
 	>class rbtree_iterator: std::iterator<T, std::bidirectional_iterator_tag> {
 		public:
-			typedef T	value_type;
+			typedef T					value_type;
+			typedef Comp				value_comp;
+			typedef Node<value_type>*	node_ptr;
+		protected:
+			node_ptr	_node;
+			value_comp	_comp;
+		public:
 
-			rbtree_iterator() {}
-			rbtree_iterator(const rbtree_iterator& rbi);
+			rbtree_iterator(value_comp comp = value_comp()) {
+				this->_node = NULL;
+				this->_comp = comp;
+			}
+			rbtree_iterator(const node_ptr node, value_comp comp = value_comp()) {
+				this->_node = node;
+				this->_comp = comp;
+			}
+			rbtree_iterator(const rbtree_iterator& rbi) {
+				*this = rbi;
+			}
 			rbtree_iterator& operator=(const rbtree_iterator& rbi) {
-				(void)rbi;
+				this->_node = rbi._node;
+				this->_comp = rbi._comp;
 				return *this;
 			}
-			~rbtree_iterator() {}
+			~rbtree_iterator() {
 
-			friend bool	operator==(const rbtree_iterator& lhs, const rbtree_iterator& rhs) {
-				return false;
 			}
 
+			friend bool	operator==(const rbtree_iterator& lhs, const rbtree_iterator& rhs) {
+				return lhs._node == rhs._node;
+			}
 
-			value_type	operator*(const rbtree_iterator& rbi);
-			rbtree_iterator&	operator++();
-			rbtree_iterator&	operator++(int);
-			rbtree_iterator&	operator--();
-			rbtree_iterator&	operator--(int);
+			value_type	operator*() {
+				if (this->_node == NULL)
+					throw std::runtime_error("Cannot dereference NULL iterator");
+				return this->_node->data;
+			}
+			rbtree_iterator&	operator++() {
+				if (this->_node == NULL)
+					throw std::runtime_error("Cannot increment NULL iterator");
+				// increment
+				return *this;
+			}
+			rbtree_iterator&	operator++(int) {
+				if (this->_node == NULL)
+					throw std::runtime_error("Cannot increment NULL iterator");
+				ft::rbtree_iterator<value_type> temp = *this;
+				// increment
+				return temp;
+			}
+			rbtree_iterator&	operator--() {
+				if (this->_node == NULL)
+					throw std::runtime_error("Cannot decrement NULL iterator");
+				// decrement
+				return *this;
+			}
+			rbtree_iterator&	operator--(int) {
+				if (this->_node == NULL)
+					throw std::runtime_error("Cannot decrement NULL iterator");
+				ft::rbtree_iterator<value_type> temp = *this;
+				// decrement
+				return temp;
+			}
 	};
 	template<
 		typename T
