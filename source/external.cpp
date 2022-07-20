6cfb8c37bfeb8f511be41bc59bac6869329e6777
GQDeltex <operitum@gmail.com>
2022-07-20 18:18:32 +0200

    Re-Add external tests but lower ram usage

--- source/external.cpp
+++ source/external.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <string>
 #include <deque>
-
 #ifndef USE_STD
 # define USE_STD 0
 #endif
-
-#if USE_STD //CREATE A REAL STL EXAMPLE
+#if USE_STD == 1
 	#include <map>
 	#include <stack>
 	#include <vector>
 	namespace ft = std;
 #else
-	//#include "../include/ft/map.hpp"
+	#include "../include/ft/map.hpp"
 	#include "../include/ft/stack.hpp"
 	#include "../include/ft/vector.hpp"
 #endif
 
 #include <stdlib.h>
 
-#define MAX_RAM 4294967296
+#define MAX_RAM 429496729
 #define BUFFER_SIZE 4096
 struct Buffer
 {
@@ -65,7 +63,7 @@ int main(int argc, char** argv) {
 	ft::stack<int> stack_int;
 	ft::vector<Buffer> vector_buffer;
 	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-	//ft::map<int, int> map_int;
+	ft::map<int, int> map_int;
 
 	for (int i = 0; i < COUNT; i++)
 	{
@@ -95,19 +93,19 @@ int main(int argc, char** argv) {
 
 	for (int i = 0; i < COUNT; ++i)
 	{
-		//map_int.insert(ft::make_pair(rand(), rand()));
+		map_int.insert(ft::make_pair(rand(), rand()));
 	}
 
 	int sum = 0;
 	for (int i = 0; i < 10000; i++)
 	{
-		//int access = rand();
-		//sum += map_int[access];
+		int access = rand();
+		sum += map_int[access];
 	}
 	std::cout << "should be constant with the same seed: " << sum << std::endl;
 
 	{
-		//ft::map<int, int> copy = map_int;
+		ft::map<int, int> copy = map_int;
 	}
 	MutantStack<char> iterable_stack;
 	for (char letter = 'a'; letter <= 'z'; letter++)
